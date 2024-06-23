ca2e9b286b17d3be01eeaab2dcb30600bf96daab
folujam <olusway@gmail.com>
2024-06-23 20:30:29 +0100

    init and dexdon

---
+++ model/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+creates a BaseModel
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    class BaseModel that defines all common attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        inits attributes of the BaseModel
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['created at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
+    def save(self):
+        """save method"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """returns a __dict__"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
+    def __str__(self):
+        """shows str how to use"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.to_dict())
